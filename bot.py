7bccfe32cf10d121b4f17f92f5d073c60180f362
egor5q <31898567+egor5q@users.noreply.github.com>
2020-01-03 16:34:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -317,3 +317,7 @@ def medit(message_text, chat_id, message_id, reply_markup=None, parse_mode=None)
     return bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text,
                                     reply_markup=reply_markup,
                                     parse_mode=parse_mode)
+
+
+bot.polling(none_stop=True)
+
